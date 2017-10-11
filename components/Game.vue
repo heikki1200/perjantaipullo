61d9082752d09f8173cbd3e92ebbb73ac5a29542
Heikki Lehmuskallio <heikki@richen.fi>
2017-10-11 12:24:04 +0300

    Game component update

--- components/Game.vue
+++ components/Game.vue
@@ -26,34 +26,45 @@
           return deg * Math.PI / 180
         }
         let sliceDeg = 360 / data.length
-        let lastEnd = 0
-        let margin = 20
+        let deg = 0
+        let center = canvasElement.width / 2
         let ctx = canvasElement.getContext('2d')
-        let drawPizza = () => {
-          data.map(x => {
-            ctx.fillStyle = 'rgb(' + x.color + ')'
-            ctx.strokeStyle = 'rgb(0, 0, 0)'
-            ctx.font = '20px Avenir'
-            ctx.lineWidth = 5
-            ctx.beginPath()
-            ctx.moveTo(canvasElement.width / 2, canvasElement.height / 2)
-            ctx.arc(canvasElement.width / 2, canvasElement.height / 2, canvasElement.height / 2.5, lastEnd, lastEnd + toRadians(sliceDeg), false)
-            ctx.closePath()
-            ctx.fillText(x.name, 0, margin)
-            ctx.fill()
-            ctx.stroke()
-            lastEnd += toRadians(sliceDeg)
-            margin += 20
-          })
+        let drawPizza = (color, deg) => {
           ctx.save()
+          ctx.beginPath()
+          ctx.fillStyle = 'rgb(' + color + ')'
+          ctx.strokeStyle = 'rgb(0, 0, 0)'
+          ctx.lineWidth = 5
+          ctx.moveTo(center, center)
+          ctx.arc(center, center, canvasElement.width / 2.5, toRadians(deg), toRadians(deg + sliceDeg), false)
+          ctx.lineTo(center, center)
+          ctx.fill()
+          ctx.stroke()
+          ctx.closePath()
+          ctx.restore()
+        }
+        let drawNames = (name, deg) => {
+          ctx.save()
+          ctx.translate(center, center)
+          ctx.rotate(toRadians(deg))
+          ctx.textAlign = 'right'
+          ctx.fillStyle = 'rgb(255, 255, 255)'
+          ctx.font = 'bold 26px Avenir'
+          ctx.fillText(name, canvasElement.width / 2 - 100, 10)
+          ctx.restore()
         }
         let drawBottle = (image, size) => {
+          ctx.save()
           let drawImage = new Image()
           drawImage.src = `/${image}.png`
-          ctx.drawImage(drawImage, canvasElement.width / 2 - (size / 2), canvasElement.width / 2 - (size / 2), size, size)
-          ctx.save()
+          ctx.drawImage(drawImage, center - (size / 2), center - (size / 2), size, size)
+          ctx.restore()
         }
-        drawPizza()
+        data.map(x => {
+          drawPizza(x.color, deg)
+          drawNames(x.name, deg + sliceDeg / 2)
+          deg += sliceDeg
+        })
         drawBottle('koff', 200)
       }
     }
