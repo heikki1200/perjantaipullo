f22fc7e45fa0b45a15132032869cff388cb97feb
Heikki Lehmuskallio <heikki@richen.fi>
2017-10-11 09:09:27 +0300

    Refactor Game component

--- components/Game.vue
+++ components/Game.vue
@@ -22,34 +22,29 @@
         let data = obj.value.map(x => {
           return x.data
         })
-        let names = obj.value.map(x => {
-          return x.data.name
-        })
-        let colors = obj.value.map(x => {
-          return x.data.color
-        })
-        function toRadians (deg) {
+        let toRadians = deg => {
           return deg * Math.PI / 180
         }
-        let playerDeg = 360 / names.length
+        let sliceDeg = 360 / data.length
         let lastEnd = 0
         let margin = 20
         let ctx = canvasElement.getContext('2d')
-        for (var i = 0; i < data.length; i++) {
-          ctx.fillStyle = 'rgb(' + colors[i] + ')'
+        data.map(x => {
+          ctx.fillStyle = 'rgb(' + x.color + ')'
           ctx.strokeStyle = 'rgb(0, 0, 0)'
           ctx.font = '20px Avenir'
           ctx.lineWidth = 5
           ctx.beginPath()
-          ctx.fillText(names[i], 0, margin)
           ctx.moveTo(canvasElement.width / 2, canvasElement.height / 2)
-          ctx.arc(canvasElement.width / 2, canvasElement.height / 2, canvasElement.height / 3, lastEnd, lastEnd + toRadians(playerDeg), false)
+          ctx.arc(canvasElement.width / 2, canvasElement.height / 2, canvasElement.height / 3, lastEnd, lastEnd + toRadians(sliceDeg), false)
           ctx.closePath()
+          ctx.fillText(x.name, 0, margin)
           ctx.fill()
           ctx.stroke()
-          lastEnd += toRadians(playerDeg)
+          lastEnd += toRadians(sliceDeg)
           margin += 20
-        }
+        })
+        ctx.save()
       }
     }
   }
